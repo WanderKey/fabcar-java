81fa4d78b1eef1d4cbf0c93c8762de2fd7c3ceca
Qi <thuwangqi14@163.com>
2023-10-16 09:43:29 +0800

    project initialization

---
+++ src/main/java/com/tsinghua/fabcar/FabcarApplication.java
@@ -0,0 +1,13 @@
+package com.tsinghua.fabcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabcarApplication.class, args);
+    }
+
+}
